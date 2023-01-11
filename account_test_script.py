4e7d6a26c307346d61903983a89968c8622c2755
borisdev <boris.dev@gmail.com>
2023-01-11 11:02:38 -0800

    initial

---
+++ account_test_script.py
@@ -0,0 +1,147 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import polling2
+from dataclasses import dataclass
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3.middleware import construct_sign_and_send_raw_middleware
+from web3.auto import Web3
+from web3.exceptions import TransactionNotFound
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+chuck = os.environ.get("CHUCKS_PUBLIC_ETH_ADDRESS")  # $$$$$$$$$$$$$$$$$ key
+private_key = os.environ.get("PRIVATE_KEY")  # $$$$$$$$$$$$$$$$$ key
+assert private_key is not None, "You must set PRIVATE_KEY environment variable"
+assert private_key.startswith("0x"), "Private key must start with 0x hex prefix"
+
+
+ALCHEMY_AUTH_TOKEN = os.environ.get("ALCHEMY_AUTH_TOKEN")
+
+
+@dataclass
+class Network:
+    """Class for keeping track of info on a web3 network."""
+    name: str
+    rpc_endpoint: str
+    block_explorer: str
+
+    def web_socket_connection(self):
+        wss = f"wss://{self.rpc_endpoint}{ALCHEMY_AUTH_TOKEN}"
+        web3 = Web3(Web3.WebsocketProvider(wss))
+        return web3
+
+
+eth_mainnet = Network("Ethereum Mainnet", "eth-mainnet.g.alchemy.com/v2/", "https://etherscan.io/")
+eth_testnet = Network("Goerli Testnet", "eth-goerli.g.alchemy.com/v2/", "https://goerli.etherscan.io/")  # Faucet: https://goerlifaucet.com/
+
+
+# *******
+# TODO: make this a CLI arg
+# network = eth_mainnet
+network = eth_testnet
+# *******
+
+account: LocalAccount = Account.from_key(private_key)
+
+
+web3 = network.web_socket_connection()
+print(web3.isConnected())
+web3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+
+if network.name == "Ethereum Mainnet":
+    boris = "borisdev.eth"
+else:
+    boris = account.address
+
+# get the nonce.  Prevents one from sending the transaction twice
+nonce = web3.eth.getTransactionCount(boris)
+
+# build a transaction in a dictionary
+tx = {
+    'nonce': nonce,
+    'to': chuck,
+    'value': 1,
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei')
+}
+
+# sign the transaction
+signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+
+addresses = {
+    "boris": boris,
+    "chuck": chuck
+}
+
+for name, address in addresses.items():
+    wei_per_eth = 10**18
+    dollars_per_eth = 1252.16
+    wei_balance = web3.eth.getBalance(address)
+    ether_balance = web3.fromWei(wei_balance, "ether")
+    balance_views = {}
+    balance_views = {
+        "wei": wei_balance,
+        # "ether by me": wei_balance / wei_per_eth,
+        "ether": ether_balance,
+        f"dollars @{dollars_per_eth} rate": float(ether_balance) * dollars_per_eth
+    }
+    print()
+    print(f"{name}'s account in {network.name}")
+    print("=========================")
+    for k, v in balance_views.items():
+        print(f"{round(v,2)} {k}")
+
+# send transaction
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+# get transaction hash
+print("toHex(tx_hash): ", web3.toHex(tx_hash))
+
+for name, address in addresses.items():
+    wei_per_eth = 10**18
+    dollars_per_eth = 1252.16
+    wei_balance = web3.eth.getBalance(address)
+    ether_balance = web3.fromWei(wei_balance, "ether")
+    balance_views = {}
+    balance_views = {
+        "wei": wei_balance,
+        # "ether by me": wei_balance / wei_per_eth,
+        "ether": ether_balance,
+        f"dollars @{dollars_per_eth} rate": float(ether_balance) * dollars_per_eth
+    }
+    print()
+    print(f"{name}'s account in {network.name}")
+    print("=========================")
+    for k, v in balance_views.items():
+        print(f"{round(v,2)} {k}")
+
+
+# Now you can use web3.eth.send_transaction(), Contract.functions.xxx.transact() functions
+# with your local private key through middleware and you no longer get the error
+# "ValueError: The method eth_sendTransaction does not exist/is not available
+
+
+# get transaction from hash returned to caller/requestor
+tx = web3.eth.getTransaction(tx_hash)
+print("tx: ", tx)
+print()
+tx_reciept = polling2.poll(
+    lambda: web3.eth.getTransactionReceipt(tx_hash),
+    ignore_exceptions=(TransactionNotFound,),
+    timeout=30,
+    step=0.5)
+print("tx_reciept: ", tx_reciept)
+
+# https://docs.python.org/3/library/queue.html
+
+# TODO: add new fields to tx reciept
+# - time span to get transaction
+# - start tx block number
+# - block number
+# - account amount before tx
+# - account amount after tx
+# - logs -- uniswap price before
+# - logs -- uniswap price after
---
+++ search_eth_mempool.py
@@ -0,0 +1,75 @@
+import os
+from os.path import join, dirname
+from web3.auto import Web3
+import asyncio
+import json
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+ALCHEMY_AUTH_TOKEN = os.environ.get("ALCHEMY_AUTH_TOKEN")
+wss = f'wss://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_AUTH_TOKEN}'
+web3 = Web3(Web3.WebsocketProvider(wss))
+
+assert web3.isConnected()
+
+# add the abi of Uniswap or Pancake Swap
+abi = json.loads('[{"inputs":[{"internalType":"address","name":"_factory","type":"address"},{"internalType":"address","name":"_WETH","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"WETH","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"amountADesired","type":"uint256"},{"internalType":"uint256","name":"amountBDesired","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"addLiquidity","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"},{"internalType":"uint256","name":"liquidity","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amountTokenDesired","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"addLiquidityETH","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"},{"internalType":"uint256","name":"liquidity","type":"uint256"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"reserveIn","type":"uint256"},{"internalType":"uint256","name":"reserveOut","type":"uint256"}],"name":"getAmountIn","outputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"reserveIn","type":"uint256"},{"internalType":"uint256","name":"reserveOut","type":"uint256"}],"name":"getAmountOut","outputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsIn","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsOut","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"reserveA","type":"uint256"},{"internalType":"uint256","name":"reserveB","type":"uint256"}],"name":"quote","outputs":[{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidity","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidityETH","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidityETHSupportingFeeOnTransferTokens","outputs":[{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityETHWithPermit","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityETHWithPermitSupportingFeeOnTransferTokens","outputs":[{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityWithPermit","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapETHForExactTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactETHForTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactETHForTokensSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForETH","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForETHSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForTokensSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"amountInMax","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapTokensForExactETH","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"amountInMax","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapTokensForExactTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]')
+
+# add an address you want to filter pending transactions for
+# make sure the address is in the correct format
+router = web3.toChecksumAddress('0x10ed43c718714eb63d5aa57b78b54704e256024e')
+Contract = web3.eth.contract(address=router, abi=abi)
+
+
+def handle_event(event):
+
+    try:
+        # remove the quotes in the transaction hash
+        transaction = Web3.toJSON(event).strip('"')
+        # use the transaction hash (that we removed the '"" from to get the details of the transaction
+        transaction = web3.eth.get_transaction(transaction)
+        # set the variable to the "to" address in the message
+        to = transaction['to']
+        # get the input data in the transaction message. This tells us what functions are going to be executed on the router
+        input_data = transaction['input']
+        # if the to address in the message is the uniswap router
+        if to == router:
+            # decode the data so you can read it
+            decode = Contract.decode_function_input(input_data)
+            # print the transaction and its details
+            # you will be able to see the functions that are passed to the router
+            # try printing the following: for function information use (decode[0]),  or for amount in/out use decode[1], or for the path use decode(['path']), or for amountIn use decode('amountIn')
+            print(decode)
+        else:
+            print('Not what we want')
+    except Exception as err:
+        # print transactions with errors. Expect to see transactions people submitted with errors
+        print(f'error: {err}')
+
+
+async def log_loop(event_filter, poll_interval):
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_event(event)
+        await asyncio.sleep(poll_interval)
+
+
+def main():
+    # filter for pending transactions
+    event_filter = web3.eth.filter('pending')
+    event_loop = asyncio.get_event_loop()
+    try:
+        event_loop.run_until_complete(
+            asyncio.gather(
+                log_loop(event_filter, 2)
+            )
+        )
+
+    finally:
+        event_loop.close()
+
+
+if __name__ == '__main__':
+    main()
