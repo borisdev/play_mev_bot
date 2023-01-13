dddb4565df9d8c464493dba1bc9551647e86217d
borisdev <boris.dev@gmail.com>
2023-01-13 14:28:16 -0800

    works

--- detect_swaps_in_mempool.py
+++ detect_swaps_in_mempool.py
@@ -1,5 +1,6 @@
 import os
-import sys
+import os.path
+import json
 import traceback
 from dataclasses import dataclass
 from os.path import join, dirname
@@ -7,7 +8,10 @@ from web3.auto import Web3
 from web3.exceptions import TransactionNotFound
 import asyncio
 from dotenv import load_dotenv
-from pancake_swap_abi import pancake_swap_abi
+from etherscan import Etherscan
+from web3.constants import WEI_PER_ETHER
+
+assert (10**18 == WEI_PER_ETHER)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -15,66 +19,118 @@ load_dotenv(dotenv_path)
 ALCHEMY_AUTH_TOKEN = os.environ.get("ALCHEMY_AUTH_TOKEN")
 wss = f'wss://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_AUTH_TOKEN}'
 node = Web3(Web3.WebsocketProvider(wss))
+etherscan = Etherscan(  # free plan allows 5 calls per second
+    os.environ.get("ETHERSCAN_API_KEY"),
+    net="main"
+)
 
 assert node.isConnected()
 
+
 @dataclass
-class Dex:
+class Router:
     """
-    Router making exchanges between two token contracts at a dynamically generated price
+    aka AMM
+    aka Router
+    makes exchanges between two token contracts at a dynamically generated price
     """
     name: str
     address: str
     abi: list
     contract: str
 
-    def __init__(self, name: str, address: str, abi: str):
+    def __init__(self, name: str, address: str):
         self.name = name
         self.address = node.toChecksumAddress(address)
-        self.contract = node.eth.contract(address=address, abi=abi)
+        self.contract = node.eth.contract(
+            address=self.address,
+            abi=self.get_abi(self.address)
+        )
 
     def contract_decode_tx(self, tx):
-        return self.contract.decode_function_input(tx['input'])
+        # print the transaction and its details
+        # you will be able to see the functions that are passed to the router
+        # try printing the following:
+        # - for function information use (decode[0])
+        # - for amount in/out use decode[1]
+        # - for the path use decode(['path'])
+        # - for amountIn use decode('amountIn')
+        # TODO: log info
+        decoded = self.contract.decode_function_input(tx['input'])
+        return decoded
+
+    def get_abi(self, address):
+        file_path = self.name + ".abi"
+        # if abi exists then load it
+        if os.path.exists(file_path):
+            with open(file_path) as f:
+                abi = json.load(f)
+                return abi
+        # if doesnt exist then fetch and save abi
+        else:
+            abi_json = etherscan.get_contract_abi(self.address)
+            abi = json.loads(abi_json)
+            with open(file_path, 'w', encoding='utf-8') as f:
+                json.dump(abi, f, ensure_ascii=False, indent=4)
+            return abi
 
 
-# routers
-targets = [
-    Dex("PancakeRouter", "0xEfF92A263d31888d860bD50809A8D171709b7b1c", pancake_swap_abi),
-    Dex("Uniswap V2: Router 2", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"),
-    Dex("Uniswap V3: Router", "0xE592427A0AEce92De3Edee1F18E0157C05861564"),
-    Dex("Uniswap V3: Router 2", "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45")
+# TODO: config yaml
+routers = [
+    Router("Uniswap_V2:_Router_2", "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"),
+    # TODO why much less volume for V3 ???
+    # Router("Uniswap_V3:_Router", "0xE592427A0AEce92De3Edee1F18E0157C05861564"),
+    # Router("Uniswap_V3:_Router_2", "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45")
 ]
-
-print(f"waiting for a pending tx to appear for {targets}")
 # "pancake_swap": '0x10ed43c718714eb63d5aa57b78b54704e256024e',  # Binance smart chain
+address2router = {router.address: router for router in routers}
+target_addresses = address2router.keys()
+
+counter = 0
 
 
-def handle_event(event):
+def handle_tx(tx):
+    """
+    general: event handling in event loop
+    specific: callback to process the node's response of a pending transaction
 
-    tx_hash = event.hex()  # transaction = Web3.toJSON(event).strip('"')
+    >>> swap
+    {
+        'amountOutMin': 2814456810484855304,
+        'deadline': 1635723516,
+        'path': ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', '0x4da08a1Bff50BE96bdeD5C7019227164b49C2bFc'],
+        'to': '0x4f9CA6eff61eFf64f43FfAFe2fA3b891c10Eb128'
+    }
+    """
+    global counter
+    tx_hash = tx.hex()
     try:
-        transaction = node.eth.get_transaction(tx_hash)
-        to = transaction['to']
-        input_data = transaction['input']
-        # print("pancake swap:", to == router)
-        if to == router_address:  # pancakeswap router
-            # print the transaction and its details
-            # you will be able to see the functions that are passed to the router
-            # try printing the following:
-            # - for function information use (decode[0])
-            # - for amount in/out use decode[1]
-            # - for the path use decode(['path'])
-            # - for amountIn use decode('amountIn')
-            print(decode)
+        tx = node.eth.get_transaction(tx_hash)
+        potential_swap_address = tx['to']
+        if potential_swap_address in target_addresses:
+            router = address2router[potential_swap_address]
+            decoded_tx = router.contract_decode_tx(tx)
+            print("====================")
+            print(router.name)
+            print("function called:", decoded_tx[0])
+            swap = decoded_tx[1]
+            try:
+                min_token_out_in_dollars = swap['amountOutMin'] / WEI_PER_ETHER * 1455
+                print("min_token_out_in_dollars", min_token_out_in_dollars)
+            except Exception as err:
+                print(err)
+            from pprint import pprint
+            pprint(swap)
             import ipdb
             ipdb.set_trace()
-            sys.exit()
-
         else:
-            print(f"Skipping since not {target}'s router address {router_address}")
+            print(f"tx count {counter} - not a target router")
+            counter = counter + 1
+            pass
     except TransactionNotFound:
-        # Expect to see transactions people submitted with errors
-        # etherscan shows not found txs as existing but many days old
+        # TODO: why do we see error "not found"?
+        # - people submit transactions with errors
+        # - strangely though etherscan show these not found txs as existing but many days old
         pass
     except Exception:
         print(traceback.format_exc())
@@ -83,23 +139,15 @@ def handle_event(event):
 
 
 async def fetch_txs_in_mempool():
-    poll_interval = 2
-    # filter_params = {
-    #     "fromBlock": "pending",
-    #     "address": router
-    # }
+    poll_interval = 1
     while True:
-        # for event in node.eth.filter(filter_params).get_new_entries():
         for event in node.eth.filter("pending").get_new_entries():
-            handle_event(event)  # add this to the stack of work, and keep going....not blocking
-        await asyncio.sleep(poll_interval)  # let event pool controller do other work now
+            handle_tx(event)  # this task to process the response doesnt block the fetch task, and vice versa
+        await asyncio.sleep(poll_interval)
 
 
 def main():
-    # python boilerplate to make an event loop so fetching event time does not block handling event time and vice versa
-    # https://medium.com/@interfacer/intro-to-async-concurrency-in-python-and-node-js-69315b1e3e36
-    # https://stackoverflow.com/questions/68139555/difference-between-async-await-in-python-vs-javascript
-    event_loop = asyncio.get_event_loop()
+    event_loop = asyncio.get_event_loop()  # one event loop started in one thread
     try:
         event_loop.run_until_complete(
             asyncio.gather(
