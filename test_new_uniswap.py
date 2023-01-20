ba24819a27233eee2a819b578bd22bcee16892fd
borisdev <boris.dev@gmail.com>
2023-01-19 17:10:27 -0800

    uniswap tx partially + leftover a big mess

--- test_new_uniswap.py
+++ test_new_uniswap.py
@@ -13,12 +13,29 @@ https://uniswap-python.com/getting-started.html
     https://github.com/uniswap-python/uniswap-python/blob/da8c7ddd615a20203615014ab658db405d41957b/uniswap/uniswap.py#L1437
 
 """
+import os
+import os.path
+import json
+import traceback
+from dataclasses import dataclass
+from os.path import join, dirname
+from web3.auto import Web3
+from web3.exceptions import TransactionNotFound
+import asyncio
+from dotenv import load_dotenv
+from etherscan import Etherscan
+from web3.constants import WEI_PER_ETHER
+import settings
 import json
 import time
 from uniswap import Uniswap
 import settings
 import constants
 
+etherscan = Etherscan(  # free plan allows 5 calls per second
+    os.environ.get("ETHERSCAN_API_KEY"),
+    net="main"
+)
 
 assert settings.USE_GOERLI is False
 address = None          # or None if you're not going to make transactions
@@ -63,7 +80,7 @@ balance = web3.eth.get_balance(sender_address)
 print("This address has:", web3.fromWei(balance, "ether"), "ETH")
 gas = 50000
 gasPrice = 60 * (10**9)
-value = 1
+value = int((1 / constants.dollars_per_eth) * 10**18)
 total = (gas * gasPrice) + value
 print("total token value trading with in dollars:", value / 10**18 * constants.dollars_per_eth)
 print("total gas in dollars:", gas * gasPrice / 10**18 * constants.dollars_per_eth)
@@ -72,6 +89,65 @@ print("total needed for tx in eth:", total / 10**18)
 print("total needed for tx in dollars:", total / 10**18 * rate)
 token_to_buy = web3.toChecksumAddress(dai)
 
+from dataclasses import dataclass
+@dataclass
+class Router:
+    """
+    aka AMM
+    aka Router
+    makes exchanges between two token contracts at a dynamically generated price
+    """
+    name: str
+    address: str
+    abi: list
+    contract: str
+
+    def __init__(self, name: str, address: str):
+        self.name = name
+        self.address = web3.toChecksumAddress(address)
+        self.contract = web3.eth.contract(
+            address=self.address,
+            abi=self.get_abi(self.address)
+        )
+
+    def contract_decode_tx(self, tx):
+        # print the transaction and its details
+        # you will be able to see the functions that are passed to the router
+        # try printing the following:
+        # - for function information use (decode[0])
+        # - for amount in/out use decode[1]
+        # - for the path use decode(['path'])
+        # - for amountIn use decode('amountIn')
+        # TODO: log info
+        decoded = self.contract.decode_function_input(tx['input'])
+        return decoded
+
+    def get_abi(self, address):
+        file_path = self.name + ".abi"
+        # if abi exists then load it
+        if os.path.exists(file_path):
+            with open(file_path) as f:
+                abi = json.load(f)
+                return abi
+        # if doesnt exist then fetch and save abi
+        else:
+            abi_json = etherscan.get_contract_abi(self.address)
+            abi = json.loads(abi_json)
+            with open(file_path, 'w', encoding='utf-8') as f:
+                json.dump(abi, f, ensure_ascii=False, indent=4)
+            return abi
+
+router = Router("DAI", dai)
+# https://ethereum.stackexchange.com/questions/99890/get-balance-of-a-specific-token-in-python-using-web3-py-on-polygon-network
+token = web3.eth.contract(address=dai, abi=router.get_abi(dai))  # declaring the token contract
+token_balance = token.functions.balanceOf(address).call()  # returns int with balance, without decimals
+print("My DAI token balance:", token_balance)
+print(token.functions.name().call())
+print(token.functions.symbol().call())
+print(token.functions.balanceOf(address).call())
+print("my address:", address)
+import ipdb
+ipdb.set_trace()
 # specifty token to spend (WETH mainnet)
 spend = web3.toChecksumAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 nonce = web3.eth.getTransactionCount(sender_address)
@@ -94,3 +170,19 @@ sign_tx = web3.eth.account.sign_transaction(
 )
 tx_hash = web3.eth.send_raw_transaction(sign_tx.rawTransaction)
 print(web3.toHex(tx_hash))
+"""
+### RESULTING OUTPUT
+
+amount of dai for 1 eth: 1546
+amount eth for 1 dai 1
+my wei balance: 4921475458126492
+my eth balance: 0.004921475458126492
+my dollar balance: 6.39791809556444
+This address has: 0.004921475458126492 ETH
+total token value trading with in dollars: 1.2521600000000002e-15
+total gas in dollars: 3.7564800000000003
+total needed for tx total wei: 3000000000000001
+total needed for tx in eth: 0.003000000000000001
+total needed for tx in dollars: 3.7564800000000016
+0xfd3597126985e19ec6a7fb84ed03d7e89facc02a55b97b0166bddd10441dbb2e
+"""
